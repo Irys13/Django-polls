e68d924769260cec3c923ea25c7c09f59bed3fab
Evie <ewe727@gmail.com>
2018-05-10 20:42:33 +0100

    half way through tutorial 5

--- irys/polls/views.py
+++ irys/polls/views.py
@@ -3,15 +3,16 @@ from .models import Choice, Question
 from django.shortcuts import get_object_or_404, render
 from django.urls import reverse
 from django.views import generic
+from django.utils import timezone
 # Create your views here.
 
 class IndexView(generic.ListView):
 	template_name = 'polls/index.html'
 	context_object_name = 'latest_question_list'
 
-	def get queryset)self:
-		"""Return the last five published questions"""
-		returm Question.objects.order_by('-pub_date')[:5]
+	def get_queryset(self):
+		"""Return the last five published questions(not including those set to be published in the future."""
+		return Question.objects.filter(pub_date__lte=timezone.now()_.order_by('-pub_date')[:5]
 
 class DetailView(generic.DetailView):
 	model = Question
@@ -22,7 +23,7 @@ class ResultsView(generic.DetailView):
 	template_name = 'polls/detail.html'
 
 class ResultsView(generic.DetailView):
-	model = Quesiton
+	model = Question
 	template_name = 'polls/results.html'
 
 
